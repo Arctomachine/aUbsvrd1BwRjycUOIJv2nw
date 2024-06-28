c5cc55117f2d88649f6fa988841761f68013b0c0
Alexander Gerasimov <2632391-Arctomachine@users.noreply.gitlab.com>
2024-06-28 11:35:24 +0300

    модальное окно дексктоп

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const API_KEY = ''
 
 const header = document.querySelector('.header')
 const searchButtonMobile = document.querySelector('#search-button-mobile')
+const modal = document.querySelector('.details-modal')
+const closeButton = document.querySelector('.details-modal .close')
 
 searchButtonMobile.addEventListener('click', () => {
 	header.classList.toggle('mobile-search')
+})
+
+closeButton.addEventListener('click', () => {
+	modal.close()
 })
